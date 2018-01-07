b6b5a3414e71b1dc22814dcbc54d17a5b4e182a2
HENRY PEYRONNIN <henry.peyronnin@tufts.edu>
2018-01-07 17:25:50 -0500

    add functions to calculator

---
+++ calculator.js
@@ -0,0 +1,16 @@
+// Define multiply
+function multiply(a,b) { 
+	return (a * b)
+}
+// Define add
+function add(a,b) { 
+	return (a + b)
+}
+// Define subtract
+function subtract(a,b) { 
+	return (a - b)
+}
+// Define divide
+function divide(a,b) { 
+	return (a / b)
+}
