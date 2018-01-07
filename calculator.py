ca0213e5703f12d9f92119b8ab2171bca63a9c82
HENRY PEYRONNIN <henry.peyronnin@tufts.edu>
2018-01-07 17:21:34 -0500

    added new functions to calculator

--- calculator.py
+++ calculator.py
@@ -10,6 +10,19 @@ def subtract(a,b):
 def divide(a,b):
 	return a / b
 
+def square(a):
+	return a**2
+
+def cube(a):
+	return(a,3)
+
+def square_n_times(number,n):
+	return((number**2)**n)
+
 print "I'm going to use the calculator functions to multiply 5 and 6"
 x = multiply(5,6)
 print x
+
+print "I'm going to square 3 4 times"
+y = square_n_times(3,4)
+print y
