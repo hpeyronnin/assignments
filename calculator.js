219efe693576445d1049782875656c95d5ef9ebf
HENRY PEYRONNIN <henry.peyronnin@tufts.edu>
2018-01-07 17:28:19 -0500

    called the function and assigned output to variable x

--- calculator.js
+++ calculator.js
@@ -14,3 +14,7 @@ function subtract(a,b) {
 function divide(a,b) { 
 	return (a / b)
 }
+
+console.log("I'm going use the calculator functions to multiply 4 and 2000")
+var x = multiply(4,2000)
+console.log(x)
